01c9bee4b878522e3684e47018a912ac49ed9351
gowthamkish <gowthamkish@outlook.com>
2024-02-20 23:34:32 +0530

    route.ts

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -1,8 +1,19 @@
 import connectDB from "@/utils/db";
 import PostModel from "@/models/PostModel";
+import { NextResponse } from "next/server";
 
-export async function GET(req: any) {
-  await connectDB();
-  const postData = await PostModel.find({});
-  return Response.json(postData);
-}
+export const GET = async (req: Request, res: Response) => {
+  try {
+    await connectDB();
+    const postData = await PostModel.find({});
+    return NextResponse.json({ message: "OK", postData }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json(
+      {
+        message: "Error",
+        error,
+      },
+      { status: 500 }
+    );
+  }
+};
